cc8c5febe0555f94ba701b05566220204cd3730b
bao238890 <lethithanh1983phd@gmail.com>
2024-06-17 15:53:57 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace CSharp_TinhLaiNganHang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSotien = new System.Windows.Forms.TextBox();
+            this.lsbkq = new System.Windows.Forms.ListBox();
+            this.txtlaisuat = new System.Windows.Forms.TextBox();
+            this.txtSonam = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập Số tiền";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Location = new System.Drawing.Point(234, 288);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(186, 64);
+            this.btnTinh.TabIndex = 1;
+            this.btnTinh.Text = "TÍNH";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nhập Lãi suất";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(198, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nhập Số Năm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(198, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Kết quả";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(105, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(575, 31);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "ỨNG DỤNG TÍNH LÃI SUẤT NGÂN HÀNG ";
+            // 
+            // txtSotien
+            // 
+            this.txtSotien.Location = new System.Drawing.Point(299, 81);
+            this.txtSotien.Name = "txtSotien";
+            this.txtSotien.Size = new System.Drawing.Size(209, 20);
+            this.txtSotien.TabIndex = 10;
+            // 
+            // lsbkq
+            // 
+            this.lsbkq.FormattingEnabled = true;
+            this.lsbkq.Location = new System.Drawing.Point(299, 173);
+            this.lsbkq.Name = "lsbkq";
+            this.lsbkq.Size = new System.Drawing.Size(209, 95);
+            this.lsbkq.TabIndex = 12;
+            this.lsbkq.SelectedIndexChanged += new System.EventHandler(this.lsbkq_SelectedIndexChanged);
+            // 
+            // txtlaisuat
+            // 
+            this.txtlaisuat.Location = new System.Drawing.Point(299, 111);
+            this.txtlaisuat.Name = "txtlaisuat";
+            this.txtlaisuat.Size = new System.Drawing.Size(209, 20);
+            this.txtlaisuat.TabIndex = 13;
+            // 
+            // txtSonam
+            // 
+            this.txtSonam.Location = new System.Drawing.Point(299, 137);
+            this.txtSonam.Name = "txtSonam";
+            this.txtSonam.Size = new System.Drawing.Size(209, 20);
+            this.txtSonam.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSonam);
+            this.Controls.Add(this.txtlaisuat);
+            this.Controls.Add(this.lsbkq);
+            this.Controls.Add(this.txtSotien);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSotien;
+        private System.Windows.Forms.ListBox lsbkq;
+        private System.Windows.Forms.TextBox txtlaisuat;
+        private System.Windows.Forms.TextBox txtSonam;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+ namespace CSharp_TinhLaiNganHang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            double tienlai;
+            double sotien = double.Parse(txtSotien.Text);
+            double laisuat = double.Parse(txtlaisuat.Text.Replace("%", "")) / 100;
+            int sonam = int.Parse(txtSonam.Text);
+            for (int i = 1;i <= 20; i++)
+            {
+                tienlai = sotien * laisuat;
+                lsbkq.Items.Add("Năm: "+i+" số tiền là: "+tienlai);
+                sotien += tienlai;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            }
+
+        private void lsbkq_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
